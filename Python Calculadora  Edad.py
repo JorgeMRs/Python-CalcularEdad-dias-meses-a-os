832e3ccc3044bcba27f737c7835fbf2052790bbf
JorgeMRs <110149668+JorgeMRs@users.noreply.github.com>
2022-12-14 00:10:42 -0300

    Update Python Calculadora  Edad.py
    Le agregue comentarios para mayor entendimiento

--- Python Calculadora  Edad.py
+++ Python Calculadora  Edad.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+# CABE ACLARAR QUE ESTA CALCULADORA ESTA ECHA PARA EL SUPUESTO CASO DE QUE TODOS LOS MESES TUBIERAN 30 DIAS 
 """
 Created on Mon Dec  5 22:43:44 2022
 
@@ -10,12 +12,12 @@ def calcular_edad(dia_nacio:int, mes_nacio:int,anio_nacio:int,dia_actual:int,mes
     DN = dia_nacio
     AN = anio_nacio
   
-    
+    #SE PASA TODO A DIAS
     dias_naci  = (DN + ((MN - 1) * 30)) + ( (360 * anio_nacio) -1 )
 
     dias_acual = (dia_actual + ((mes_actual - 1) * 30)) + ( (360 * anio_actual) -1 )
     
-     
+    #CANTIDAD DE DIAS SOBRANTES
     dias_sobra = dias_acual - dias_naci 
   
     años  = 0         
@@ -24,7 +26,8 @@ def calcular_edad(dia_nacio:int, mes_nacio:int,anio_nacio:int,dia_actual:int,mes
     dias  = 0
     sobras = 0
     
-    
+    #SIRVE PARA SABER SI LA CANTIDAD DE DIAS QUE SOBRAN SUMAN UN AÑO Y SI NO ES ASI LA VARIABLE "AÑO" SE IGUALARA A 0 MIENTRAS QUE ESA CANTIDAD DE DIAS SOBRANTES
+    #SE LOS DIVIDIRA POR 30 YA QUE LOS MESES EN ESTE CASO SON TODOS DE 30 DIAS, Y DESPUES LA VARIABLE "DIAS" ASUME EL VALOR DEL RESTO DE ESA DIVICION 
     if dias_sobra < 360:
         años = 0
         meses = (dias_sobra // 30) 
@@ -32,7 +35,8 @@ def calcular_edad(dia_nacio:int, mes_nacio:int,anio_nacio:int,dia_actual:int,mes
         
     else:
         
-       
+       #MIENTRAS "sobras" EQUIVALE A EL TOTAL DE DIAS SOBRANTES DE DIVIDIR "dias_sobra" entro 360", SE LE SUMARA "1" A LA VARIABLE "meses" Y SE LE
+       #RESTARA 30 DIAS A "sobras" 
             años = dias_sobra // 360
             sobras = dias_sobra - (años * 360)
             while sobras // 30 >=1 :
